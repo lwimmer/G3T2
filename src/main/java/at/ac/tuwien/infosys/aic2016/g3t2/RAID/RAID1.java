00628a8e4e0e96f93a611f3058d7c6c8de0fe645
Florian Pritz <bluewind@xinu.at>
2016-11-04 21:52:11 +0100

    Implement RAID1
    Signed-off-by: Florian Pritz <bluewind@xinu.at>

--- src/main/java/at/ac/tuwien/infosys/aic2016/g3t2/RAID/RAID1.java
+++ src/main/java/at/ac/tuwien/infosys/aic2016/g3t2/RAID/RAID1.java
@@ -1,4 +1,79 @@
 package at.ac.tuwien.infosys.aic2016.g3t2.RAID;
 
-public class RAID1 {
+import at.ac.tuwien.infosys.aic2016.g3t2.Blobstore.AWS;
+import at.ac.tuwien.infosys.aic2016.g3t2.Blobstore.Blob;
+import at.ac.tuwien.infosys.aic2016.g3t2.Blobstore.IBlobstore;
+import at.ac.tuwien.infosys.aic2016.g3t2.Blobstore.Location;
+import at.ac.tuwien.infosys.aic2016.g3t2.exceptions.ItemMissingException;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class RAID1 implements IRAID {
+
+    private final List<IBlobstore> blobstores;
+
+    public RAID1(List<IBlobstore> blobstores) {
+        this.blobstores = blobstores;
+    }
+
+    public RAID1() {
+        this.blobstores = new ArrayList<>();
+        this.blobstores.add(new AWS());
+    }
+
+    @Override
+    public boolean create(String storagefilename, byte[] data) {
+        boolean success = false;
+        for (IBlobstore bs : this.blobstores) {
+            boolean result = bs.create(storagefilename, data);
+            if (result) {
+                success = true;
+            }
+        }
+        return success;
+    }
+
+    @Override
+    public boolean delete(String storagefilename) throws ItemMissingException {
+        for (IBlobstore bs : this.blobstores) {
+            boolean result = bs.delete(storagefilename);
+            if (!result) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public File read(String storagefilename) throws ItemMissingException {
+        ArrayList<Location> locations = new ArrayList<>();
+        byte[] data = null;
+
+        for (IBlobstore bs : this.blobstores) {
+            try {
+                Blob blob = bs.read(storagefilename);
+                locations.add(blob.getLocation2());
+                data = blob.getData();
+            } catch (ItemMissingException e) {
+                // TODO recover the broken copy?
+            }
+        }
+
+        if (data == null) {
+            throw new ItemMissingException();
+        }
+        return new File(data, locations);
+    }
+
+    @Override
+    public List<String> listFiles() {
+        Set<String> result = new LinkedHashSet<>();
+        for (IBlobstore bs : this.blobstores) {
+            result.addAll(bs.listBlobs());
+        }
+        return new ArrayList<>(result);
+    }
 }
--- src/test/java/at/ac/tuwien/infosys/aic2016/g3t2/RAID/RAID1Test.java
+++ src/test/java/at/ac/tuwien/infosys/aic2016/g3t2/RAID/RAID1Test.java
@@ -1,28 +1,178 @@
 package at.ac.tuwien.infosys.aic2016.g3t2.RAID;
 
+import at.ac.tuwien.infosys.aic2016.g3t2.Blobstore.Blob;
+import at.ac.tuwien.infosys.aic2016.g3t2.Blobstore.IBlobstore;
+import at.ac.tuwien.infosys.aic2016.g3t2.exceptions.ItemMissingException;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
 public class RAID1Test {
-    @Test
-    public void create() throws Exception {
+    private IBlobstore bs1;
+    private IBlobstore bs2;
+    private IBlobstore bs3;
 
+    @Before
+    public void setUp() {
+        this.bs1 = Mockito.mock(IBlobstore.class);
+        this.bs2 = Mockito.mock(IBlobstore.class);
+        this.bs3 = Mockito.mock(IBlobstore.class);
+    }
+
+    @After
+    public void tearDown() {
+        Mockito.verifyNoMoreInteractions(bs1);
+        Mockito.verifyNoMoreInteractions(bs2);
+        Mockito.verifyNoMoreInteractions(bs3);
+
+        bs1 = null;
+        bs2 = null;
+        bs3 = null;
     }
 
     @Test
-    public void delete() throws Exception {
+    public void create_shouldWorkSimpleCase() throws Exception {
+        byte[] data = "blub".getBytes();
 
+        RAID1 r = new RAID1(Arrays.asList(bs1));
+
+        Mockito.when(bs1.create("foo", data)).thenReturn(true);
+        assertEquals(true, r.create("foo", data));
+        Mockito.verify(bs1).create("foo", data);
     }
 
     @Test
-    public void read() throws Exception {
+    public void create_shouldWorkIfSomeBlobstoresFail() throws Exception {
+        byte[] data = "blub".getBytes();
 
+        RAID1 r = new RAID1(Arrays.asList(bs1, bs2, bs3));
+
+        Mockito.when(bs1.create("foo", data)).thenReturn(false);
+        Mockito.when(bs2.create("foo", data)).thenReturn(true);
+        Mockito.when(bs3.create("foo", data)).thenReturn(false);
+
+        assertEquals(true, r.create("foo", data));
+
+        Mockito.verify(bs1).create("foo", data);
+        Mockito.verify(bs2).create("foo", data);
+        Mockito.verify(bs3).create("foo", data);
+    }
+    @Test
+    public void create_shouldErrorIfAllBlobstoresFail() throws Exception {
+        byte[] data = "blub".getBytes();
+
+        RAID1 r = new RAID1(Arrays.asList(bs1, bs2));
+
+        Mockito.when(bs1.create("foo", data)).thenReturn(false);
+        Mockito.when(bs2.create("foo", data)).thenReturn(false);
+
+        assertEquals(false, r.create("foo", data));
+
+        Mockito.verify(bs1).create("foo", data);
+        Mockito.verify(bs2).create("foo", data);
     }
 
     @Test
-    public void listFiles() throws Exception {
+    public void read_shouldWorkSimpleCase() throws Exception {
+        byte[] data = "blub".getBytes();
+        Blob blob = new Blob();
+        blob.setData(data);
 
+        RAID1 r = new RAID1(Arrays.asList(bs1));
+
+        Mockito.when(bs1.read("foo")).thenReturn(blob);
+        assertEquals(data, r.read("foo").getData());
+        Mockito.verify(bs1).read("foo");
     }
 
+    @Test
+    public void read_shouldWorkIfMissingOnSomeBlobstores() throws Exception {
+        byte[] data = "blub".getBytes();
+        Blob blob = new Blob();
+        blob.setData(data);
+
+        RAID1 r = new RAID1(Arrays.asList(bs1, bs2, bs3));
+
+        Mockito.when(bs1.read("foo")).thenThrow(ItemMissingException.class);
+        Mockito.when(bs2.read("foo")).thenReturn(blob);
+        Mockito.when(bs3.read("foo")).thenThrow(ItemMissingException.class);
+
+        assertEquals(data, r.read("foo").getData());
+
+        Mockito.verify(bs1).read("foo");
+        Mockito.verify(bs2).read("foo");
+        Mockito.verify(bs3).read("foo");
+    }
+
+    @Test
+    public void read_shouldErrorIfAllMissing() throws Exception {
+        RAID1 r = new RAID1(Arrays.asList(bs1));
+
+        Mockito.when(bs1.read("foo2")).thenThrow(ItemMissingException.class);
+
+        try {
+            r.read("foo2");
+            fail("should throw exception");
+        } catch (ItemMissingException e) {
+        }
+
+        Mockito.verify(bs1).read("foo2");
+    }
+
+    @Test
+    public void delete_shouldWorkSimpleCase() throws Exception {
+        RAID1 r = new RAID1(Arrays.asList(bs1, bs2, bs3));
+
+        Mockito.when(bs1.delete("foo2")).thenReturn(true);
+        Mockito.when(bs2.delete("foo2")).thenReturn(true);
+        Mockito.when(bs3.delete("foo2")).thenReturn(true);
+
+        assertEquals(true, r.delete("foo2"));
+
+        Mockito.verify(bs1).delete("foo2");
+        Mockito.verify(bs2).delete("foo2");
+        Mockito.verify(bs3).delete("foo2");
+    }
+
+    @Test
+    public void delete_shouldErrorFirstFailure() throws Exception {
+        RAID1 r = new RAID1(Arrays.asList(bs1, bs2));
+
+        Mockito.when(bs1.delete("foo2")).thenReturn(false);
+        Mockito.when(bs2.delete("foo2")).thenReturn(false);
+
+        assertEquals(false, r.delete("foo2"));
+
+        Mockito.verify(bs1).delete("foo2");
+        Mockito.verify(bs2, times(0)).delete("foo2");
+    }
+
+    @Test
+    public void listFiles_shouldWork() throws Exception {
+        List<String> list1 = Arrays.asList();
+        List<String> list2 = Arrays.asList("foo", "bar");
+        List<String> list3 = Arrays.asList("blub", "bar");
+        List<String> result = Arrays.asList("foo", "bar", "blub");
+
+        RAID1 r = new RAID1(Arrays.asList(bs1, bs2, bs3));
+        Mockito.when(bs1.listBlobs()).thenReturn(list1);
+        Mockito.when(bs2.listBlobs()).thenReturn(list2);
+        Mockito.when(bs3.listBlobs()).thenReturn(list3);
+
+        assertEquals(result, r.listFiles());
+
+        Mockito.verify(bs1).listBlobs();
+        Mockito.verify(bs2).listBlobs();
+        Mockito.verify(bs3).listBlobs();
+    }
 }
