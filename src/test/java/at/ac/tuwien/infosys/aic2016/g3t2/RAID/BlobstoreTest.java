82caf5ac39574b9276640d7da774195a4538f5ee
Leonhard Wimmer <leo@lww.at>
2016-12-12 14:28:10 +0100

    more generic BlobstoreTest (no dep. on specific impl.)

--- src/test/java/at/ac/tuwien/infosys/aic2016/g3t2/RAID/BlobstoreTest.java
+++ src/test/java/at/ac/tuwien/infosys/aic2016/g3t2/RAID/BlobstoreTest.java
@@ -1,46 +1,29 @@
 package at.ac.tuwien.infosys.aic2016.g3t2.RAID;
 
-import at.ac.tuwien.infosys.aic2016.g3t2.Blobstore.AWS;
-import at.ac.tuwien.infosys.aic2016.g3t2.Blobstore.Box;
-import at.ac.tuwien.infosys.aic2016.g3t2.Blobstore.Dropbox;
-import at.ac.tuwien.infosys.aic2016.g3t2.Blobstore.IBlobstore;
-import at.ac.tuwien.infosys.aic2016.g3t2.exceptions.ItemMissingException;
-import org.junit.After;
-import org.junit.Before;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.*;
-
-import java.util.Arrays;
-import java.util.List;
+import at.ac.tuwien.infosys.aic2016.g3t2.Blobstore.IBlobstore;
+import at.ac.tuwien.infosys.aic2016.g3t2.exceptions.ItemMissingException;
+import junitx.framework.ListAssert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class BlobstoreTest {
+    
+    @Autowired
     private List<IBlobstore> bs;
-
-    @Autowired
-    private AWS aws;
-
-    @Autowired
-    private Dropbox dropbox;
-
-    @Autowired
-    private Box box;
-
-    @Before
-    public void setUp() {
-        this.bs = Arrays.asList(aws, dropbox, box);
-    }
-
-    @After
-    public void tearDown() {
-        this.bs = null;
-    }
 
     @Test
     public void create_shouldWorkSimpleCase() throws Exception {
@@ -82,9 +65,10 @@ public class BlobstoreTest {
         byte[] data = "blub".getBytes();
 
         for (IBlobstore bs : this.bs) {
+            List<String> initialList = bs.listBlobs();
             assertEquals(true, bs.create("foo-test-delete-1", data));
             assertEquals(true, bs.delete("foo-test-delete-1"));
-            assertEquals(Arrays.asList(), bs.listBlobs());
+            ListAssert.assertEquals(initialList, bs.listBlobs());
         }
     }
 
@@ -102,15 +86,20 @@ public class BlobstoreTest {
 
     @Test
     public void listFiles_shouldWork() throws Exception {
+        
         List<String> list = Arrays.asList("bar-4271", "foo-4271");
         byte[] data = "blub".getBytes();
 
         for (IBlobstore bs : this.bs) {
+            List<String> initialList = bs.listBlobs();
             bs.create("bar-4271", data);
             bs.create("foo-4271", data);
-            assertEquals(list, bs.listBlobs());
+            List<String> expect = new ArrayList<>(initialList);
+            expect.addAll(list);
+            ListAssert.assertEquals(expect, bs.listBlobs());
             bs.delete("foo-4271");
             bs.delete("bar-4271");
+            ListAssert.assertEquals(initialList, bs.listBlobs());
         }
     }
 }
