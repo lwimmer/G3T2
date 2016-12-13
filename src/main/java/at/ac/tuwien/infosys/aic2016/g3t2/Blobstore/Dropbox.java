99a77224449c7b8efe2a4f8345730f8cc1f127b4
Baris <Baris@t195-063.demo.tuwien.ac.at>
2016-12-13 11:39:43 +0100

    Dropbox: logger added

--- src/main/java/at/ac/tuwien/infosys/aic2016/g3t2/Blobstore/Dropbox.java
+++ src/main/java/at/ac/tuwien/infosys/aic2016/g3t2/Blobstore/Dropbox.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
-import com.dropbox.core.v2.files.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,11 @@ import com.dropbox.core.DbxHost;
 import com.dropbox.core.DbxRequestConfig;
 import com.dropbox.core.util.IOUtil;
 import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.DeleteErrorException;
+import com.dropbox.core.v2.files.DownloadErrorException;
+import com.dropbox.core.v2.files.FileMetadata;
+import com.dropbox.core.v2.files.Metadata;
+import com.dropbox.core.v2.files.WriteMode;
 
 import at.ac.tuwien.infosys.aic2016.g3t2.exceptions.ItemMissingException;
 
@@ -32,16 +38,21 @@ public class Dropbox implements IBlobstore {
 
 	private static final String DEFAULT_STORAGE_PATH = "/";
 
-	@Value("${dropbox.accessToken}") private String accessToken;
-	
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Value("${dropbox.accessToken}")
+	private String accessToken;
+
 	@PostConstruct
 	private void init() {
 		// Reading auth-file
+		logger.info("Getting Dropbox auth info");
 		DbxAuthInfo authInfo = new DbxAuthInfo(accessToken, DbxHost.DEFAULT);
-		
+
 		// Creating a DbxClientV2 to make API calls
 		DbxRequestConfig requestConfig = new DbxRequestConfig("DropboxAPI/2.1.1");
 		dbxClient = new DbxClientV2(requestConfig, authInfo.getAccessToken(), authInfo.getHost());
+		logger.info("Dropbox Client is created with the given auth info");
 	}
 
 	/**
@@ -51,13 +62,11 @@ public class Dropbox implements IBlobstore {
 	public boolean create(String blobname, byte[] data) {
 		InputStream input = new ByteArrayInputStream(data);
 
-		// Checks if the data is a part of the file. Data is uploaded in a single request.
+		// Checks if the data is a part of the file. Data is uploaded in a
+		// single request.
 		try {
-			FileMetadata metadata = dbxClient.files().uploadBuilder(DEFAULT_STORAGE_PATH + blobname)
-					.withMode(WriteMode.OVERWRITE).withClientModified(new Date()).uploadAndFinish(input);
-			System.out.println("**********DROPBOX***********");
-			System.out.println("Blob is uploaded to Dropbox. \nName: " + blobname + "\nSize: " + metadata.getSize());
-			System.out.println("****************************");
+			dbxClient.files().uploadBuilder(DEFAULT_STORAGE_PATH + blobname).withMode(WriteMode.OVERWRITE)
+					.withClientModified(new Date()).uploadAndFinish(input);
 		} catch (DbxException | IOException e) {
 			e.printStackTrace();
 			return false;
@@ -72,9 +81,6 @@ public class Dropbox implements IBlobstore {
 	public boolean delete(String blobname) throws ItemMissingException {
 		try {
 			dbxClient.files().delete(DEFAULT_STORAGE_PATH + blobname);
-			System.out.println("**********DROPBOX***********");
-			System.out.println("Blob is deleted. \nName: " + blobname);
-			System.out.println("****************************");
 		} catch (DeleteErrorException e) {
 			if (e.errorValue.getPathLookupValue().isNotFound()) {
 				throw new ItemMissingException();
@@ -95,10 +101,10 @@ public class Dropbox implements IBlobstore {
 	public Blob read(String blobname) throws ItemMissingException {
 		try {
 			DbxDownloader<FileMetadata> downloader = dbxClient.files().download(DEFAULT_STORAGE_PATH + blobname);
-			FileMetadata metadata = downloader.getResult();
 
 			InputStream inputStream = downloader.getInputStream();
 			byte[] data = IOUtil.slurp(inputStream, IOUtil.DEFAULT_COPY_BUFFER_SIZE);
+
 			return new Blob(data);
 		} catch (DownloadErrorException e) {
 			if (e.errorValue.isPath() && e.errorValue.getPathValue().isNotFound()) {
