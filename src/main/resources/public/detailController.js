107d0e2890959373ae32bc6eb29f431f1dd6d283
Laura Fagagnini <e1302472@student.tuwien.ac.at>
2017-01-20 03:45:09 +0100

    basic structure web-UI: list, upload, delete, download

---
+++ src/main/resources/public/detailController.js
@@ -0,0 +1,28 @@
+(function () {
+
+  //  var app = angular.module("app", []);
+
+    app.controller("DetailController", ["$http", "$scope", "giveFile", "$window", "$localStorage", function($http, $scope, giveFile, $window, $localStorage) {
+    	
+    	var filename = $localStorage.filename; 
+    	
+        $scope.filename = filename;
+        
+        var URL = '/file/'+filename;
+    	
+        $http.get(URL, {
+ //       	params: { filename}
+        })
+        .then(function(response) {
+            $scope.file = response.data;
+        });
+        
+        console.log($scope.file);
+        
+        $scope.goBack = function() {
+        	$window.location.href = '/files.html';
+        }
+        
+    } ]); 
+
+})();
