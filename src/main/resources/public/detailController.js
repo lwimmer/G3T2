f833081e4a66385e005b9c5c51f5ba5a1812c8cb
Laura Fagagnini <e1302472@student.tuwien.ac.at>
2017-01-20 18:03:49 +0100

    Detail-Seite mit /metadata befüllt

--- src/main/resources/public/detailController.js
+++ src/main/resources/public/detailController.js
@@ -8,15 +8,22 @@
     	
         $scope.filename = filename;
         
-        var URL = '/file/'+filename;
+        var URL = '/file/'+filename+'/metadata';
     	
         $http.get(URL, {
         })
         .then(function(response) {
-            $scope.file = response.data;
-        });
+        	console.log("in then");
+        	console.log(response.data);
+            $scope.versions = response.data;
+            var str = response.data.locations[0].filename;
+            $scope.name = str.split("_");
+        }, function errorCallback(response) {
+        	alert("Reading versions failed");
+        	console.log(response.data);
+         });
         
-        console.log($scope.file);
+        console.log($scope.versions);
         
         $scope.goBack = function() {
         	$window.location.href = '/files.html';
