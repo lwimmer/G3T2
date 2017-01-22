b8bb92d944b7cbcc33a7febe12f6dd168377de4c
Leonhard Wimmer <leo@lww.at>
2017-01-22 15:52:38 +0100

    allow for external credentials.properties

--- src/main/java/at/ac/tuwien/infosys/aic2016/g3t2/MainApplication.java
+++ src/main/java/at/ac/tuwien/infosys/aic2016/g3t2/MainApplication.java
@@ -3,9 +3,14 @@ package at.ac.tuwien.infosys.aic2016.g3t2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
-@PropertySource("classpath:/credentials.properties")
+@PropertySources({
+        @PropertySource(ignoreResourceNotFound = true, value = "classpath:/credentials.properties"),
+        @PropertySource(ignoreResourceNotFound = true, value = "file:credentials.properties")
+    })
+
 public class MainApplication {
 
 	public static void main(String[] args) {
